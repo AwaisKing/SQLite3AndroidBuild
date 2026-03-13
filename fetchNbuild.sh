7f7f13b50f7ca02de7c930cd66d7472b3146074c
AWAiS <awaisking.magician@gmail.com>
2026-03-13 05:27:08 +0500

    update

--- fetchNbuild.sh
+++ fetchNbuild.sh
@@ -52,30 +52,30 @@ else
   exit 1
 fi
 
-for TOOL_NAME in gperf jar java javadoc make perl php sed tar yes unzip ; do
-  if ! which "$TOOL_NAME" >/dev/null 2>&1 ; then
-    echo "Error: this script requires $TOOL_NAME tool installed."
-    exit 1
-  fi
-done
+# for TOOL_NAME in gperf jar java javadoc make perl php sed tar yes unzip ; do
+  # if ! which "$TOOL_NAME" >/dev/null 2>&1 ; then
+    # echo "Error: this script requires $TOOL_NAME tool installed."
+    # exit 1
+  # fi
+# done
 
-if [[ $(which make) = *" "* ]] ; then
-  echo "Error: OpenSSL expects that full path to make tool doesn't contain spaces. Move it to some other place."
-  exit 1
-fi
+# if [[ $(which make) = *" "* ]] ; then
+  # echo "Error: OpenSSL expects that full path to make tool doesn't contain spaces. Move it to some other place."
+  # exit 1
+# fi
 
-if ! perl -MExtUtils::MakeMaker -MLocale::Maketext::Simple -MPod::Usage -e '' >/dev/null 2>&1 ; then
-  echo "Error: Perl installation is broken."
-  if [[ "$OSTYPE" == "msys" ]] ; then
-    echo "For Git Bash you need to manually copy ExtUtils, Locale and Pod modules to /usr/share/perl5/core_perl from any compatible Perl installation."
-  fi
-  exit 1
-fi
+# if ! perl -MExtUtils::MakeMaker -MLocale::Maketext::Simple -MPod::Usage -e '' >/dev/null 2>&1 ; then
+  # echo "Error: Perl installation is broken."
+  # if [[ "$OSTYPE" == "msys" ]] ; then
+    # echo "For Git Bash you need to manually copy ExtUtils, Locale and Pod modules to /usr/share/perl5/core_perl from any compatible Perl installation."
+  # fi
+  # exit 1
+# fi
 
-if ! java --help >/dev/null 2>&1 ; then
-  echo "Error: Java installation is broken. Install JDK from https://www.oracle.com/java/technologies/downloads/ or via the package manager."
-  exit 1
-fi
+# if ! java --help >/dev/null 2>&1 ; then
+  # echo "Error: Java installation is broken. Install JDK from https://www.oracle.com/java/technologies/downloads/ or via the package manager."
+  # exit 1
+# fi
 
 # echo "Downloading SDK Manager..."
 # mkdir -p "$ANDROID_SDK_ROOT" || exit 1
@@ -97,17 +97,19 @@ fi
 ##################### my stuff #####################
 rm *.zip >/dev/null 2>&1
 
+[ -e build ] || exit 1
+
 echo "downloading sqlite3 amalgamation from: $sqliteUrl"
 $WGET "$sqliteUrl" || exit 1
 
 [ -e sqlite-build ] || mkdir -p sqlite-build
-unzip -qq "${sqliteUrl##*/}" -d sqlite-build || exit 1
+unzip -qq "${sqliteUrl##*/}" || exit 1
+mv sqlite* sqlite-build >/dev/null 2>&1 || mv *amalgamation* sqlite-build >/dev/null 2>&1
 [ -e sqlite-build ] || exit 1
-rm *.zip >/dev/null 2>&1
-# cd sqlite-build
 
-mv sqlite* build >/dev/null 2>&1 || mv *amalgamation* build >/dev/null 2>&1
-[ -e build ] || exit 1
+rm *.zip >/dev/null 2>&1
+
+# cd sqlite-build
 
 export NDK_PROJECT_PATH=$(pwd)
 
