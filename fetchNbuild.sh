5efc2a81962ca2e8c11f5e8e22303b7fb49f9ca2
AWAiS <awaisking.magician@gmail.com>
2026-03-13 05:31:42 +0500

    Update fetchNbuild.sh

--- fetchNbuild.sh
+++ fetchNbuild.sh
@@ -103,15 +103,12 @@ echo "downloading sqlite3 amalgamation from: $sqliteUrl"
 $WGET "$sqliteUrl" || exit 1
 
 unzip -qq "${sqliteUrl##*/}" || exit 1
-
-ls -l
-
+rm *.zip >/dev/null 2>&1
 mv sqlite* sqlite-build >/dev/null 2>&1 || mv *amalgamation* sqlite-build >/dev/null 2>&1
 [ -e sqlite-build ] || exit 1
 
-mv sqlite-build build/
+mv sqlite-build build
 
-rm *.zip >/dev/null 2>&1
 
 # cd sqlite-build
 
