0fdcac5baa45c5478db8efd5cbcd484fb9f04320
AWAiS <awaisking.magician@gmail.com>
2026-03-13 05:34:47 +0500

    update

--- build/jni/Android.mk
+++ build/jni/Android.mk
@@ -8,9 +8,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE            := sqlite3-a
 LOCAL_MODULE_FILENAME   := libsqlite3
-LOCAL_SRC_FILES         := ../build/sqlite3.c
-LOCAL_C_INCLUDES        := ../build
-LOCAL_EXPORT_C_INCLUDES := ../build
+LOCAL_SRC_FILES         := ../sqlite-build/sqlite3.c
+LOCAL_C_INCLUDES        := ../sqlite-build
+LOCAL_EXPORT_C_INCLUDES := ../sqlite-build
 LOCAL_CFLAGS            := -DSQLITE_THREADSAFE=1
 include $(BUILD_STATIC_LIBRARY)
 
@@ -18,9 +18,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE            := sqlite3-static-cli
 LOCAL_MODULE_FILENAME   := sqlite3-static
 LOCAL_STATIC_LIBRARIES  := libsqlite3-a
-LOCAL_SRC_FILES         := ../build/shell.c ../build/sqlite3.c
-LOCAL_C_INCLUDES        := ../build
-LOCAL_EXPORT_C_INCLUDES := ../build
+LOCAL_SRC_FILES         := ../sqlite-build/shell.c ../sqlite-build/sqlite3.c
+LOCAL_C_INCLUDES        := ../sqlite-build
+LOCAL_EXPORT_C_INCLUDES := ../sqlite-build
 LOCAL_CFLAGS            := -DSQLITE_THREADSAFE=1 -fPIE
 LOCAL_LDFLAGS           := -fPIE -pie
 include $(BUILD_EXECUTABLE)
@@ -33,9 +33,9 @@ include $(BUILD_EXECUTABLE)
 include $(CLEAR_VARS)
 LOCAL_MODULE            := sqlite3-so
 LOCAL_MODULE_FILENAME   := libsqlite3
-LOCAL_SRC_FILES         := ../build/sqlite3.c
-LOCAL_C_INCLUDES        := ../build
-LOCAL_EXPORT_C_INCLUDES := ../build
+LOCAL_SRC_FILES         := ../sqlite-build/sqlite3.c
+LOCAL_C_INCLUDES        := ../sqlite-build
+LOCAL_EXPORT_C_INCLUDES := ../sqlite-build
 LOCAL_CFLAGS            := -DSQLITE_THREADSAFE=1
 include $(BUILD_SHARED_LIBRARY)
 
@@ -43,9 +43,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE            := sqlite3-dynamic-cli
 LOCAL_MODULE_FILENAME   := sqlite3-dynamic
 LOCAL_SHARED_LIBRARIES  := libsqlite3-so
-LOCAL_SRC_FILES         := ../build/shell.c ../build/sqlite3.c
-LOCAL_C_INCLUDES        := ../build
-LOCAL_EXPORT_C_INCLUDES := ../build
+LOCAL_SRC_FILES         := ../sqlite-build/shell.c ../sqlite-build/sqlite3.c
+LOCAL_C_INCLUDES        := ../sqlite-build
+LOCAL_EXPORT_C_INCLUDES := ../sqlite-build
 LOCAL_CFLAGS            := -DSQLITE_THREADSAFE=1 -fPIE
 LOCAL_LDFLAGS           := -fPIE -pie
 include $(BUILD_EXECUTABLE)
--- fetchNbuild.sh
+++ fetchNbuild.sh
@@ -52,46 +52,46 @@ else
   exit 1
 fi
 
-# for TOOL_NAME in gperf jar java javadoc make perl php sed tar yes unzip ; do
-  # if ! which "$TOOL_NAME" >/dev/null 2>&1 ; then
-    # echo "Error: this script requires $TOOL_NAME tool installed."
-    # exit 1
-  # fi
-# done
+for TOOL_NAME in gperf jar java javadoc make perl php sed tar yes unzip ; do
+  if ! which "$TOOL_NAME" >/dev/null 2>&1 ; then
+    echo "Error: this script requires $TOOL_NAME tool installed."
+    exit 1
+  fi
+done
 
-# if [[ $(which make) = *" "* ]] ; then
-  # echo "Error: OpenSSL expects that full path to make tool doesn't contain spaces. Move it to some other place."
-  # exit 1
-# fi
+if [[ $(which make) = *" "* ]] ; then
+  echo "Error: OpenSSL expects that full path to make tool doesn't contain spaces. Move it to some other place."
+  exit 1
+fi
 
-# if ! perl -MExtUtils::MakeMaker -MLocale::Maketext::Simple -MPod::Usage -e '' >/dev/null 2>&1 ; then
-  # echo "Error: Perl installation is broken."
-  # if [[ "$OSTYPE" == "msys" ]] ; then
-    # echo "For Git Bash you need to manually copy ExtUtils, Locale and Pod modules to /usr/share/perl5/core_perl from any compatible Perl installation."
-  # fi
-  # exit 1
-# fi
+if ! perl -MExtUtils::MakeMaker -MLocale::Maketext::Simple -MPod::Usage -e '' >/dev/null 2>&1 ; then
+  echo "Error: Perl installation is broken."
+  if [[ "$OSTYPE" == "msys" ]] ; then
+    echo "For Git Bash you need to manually copy ExtUtils, Locale and Pod modules to /usr/share/perl5/core_perl from any compatible Perl installation."
+  fi
+  exit 1
+fi
 
-# if ! java --help >/dev/null 2>&1 ; then
-  # echo "Error: Java installation is broken. Install JDK from https://www.oracle.com/java/technologies/downloads/ or via the package manager."
-  # exit 1
-# fi
+if ! java --help >/dev/null 2>&1 ; then
+  echo "Error: Java installation is broken. Install JDK from https://www.oracle.com/java/technologies/downloads/ or via the package manager."
+  exit 1
+fi
 
-# echo "Downloading SDK Manager..."
-# mkdir -p "$ANDROID_SDK_ROOT" || exit 1
-# cd "$ANDROID_SDK_ROOT" || exit 1
-# $WGET "https://dl.google.com/android/repository/commandlinetools-$OS_NAME-14742923_latest.zip" || exit 1
-# mkdir -p cmdline-tools || exit 1
-# unzip -qq "commandlinetools-$OS_NAME-14742923_latest.zip" -d cmdline-tools || exit 1
-# rm "commandlinetools-$OS_NAME-14742923_latest.zip" || exit 1
-# mv cmdline-tools/* cmdline-tools/latest/ || exit 1
+echo "Downloading SDK Manager..."
+mkdir -p "$ANDROID_SDK_ROOT" || exit 1
+cd "$ANDROID_SDK_ROOT" || exit 1
+$WGET "https://dl.google.com/android/repository/commandlinetools-$OS_NAME-14742923_latest.zip" || exit 1
+mkdir -p cmdline-tools || exit 1
+unzip -qq "commandlinetools-$OS_NAME-14742923_latest.zip" -d cmdline-tools || exit 1
+rm "commandlinetools-$OS_NAME-14742923_latest.zip" || exit 1
+mv cmdline-tools/* cmdline-tools/latest/ || exit 1
 
-# echo "Installing required SDK tools..."
-# cd cmdline-tools/latest/bin/ || exit 1
-# yes | $SDKMANAGER --licenses >/dev/null || exit 1
-# $SDKMANAGER --install "ndk;$ANDROID_NDK_VERSION" "cmake;$CMAKE_VERSION" "build-tools;$BUILD_TOOLS_VERSION" "platforms;android-36" > /dev/null || exit 1
+echo "Installing required SDK tools..."
+cd cmdline-tools/latest/bin/ || exit 1
+yes | $SDKMANAGER --licenses >/dev/null || exit 1
+$SDKMANAGER --install "ndk;$ANDROID_NDK_VERSION" "cmake;$CMAKE_VERSION" "build-tools;$BUILD_TOOLS_VERSION" "platforms;android-36" > /dev/null || exit 1
 
-# cd ../../../
+cd ../../../
 
 
 ##################### my stuff #####################
@@ -108,12 +108,13 @@ mv sqlite* sqlite-build >/dev/null 2>&1 || mv *amalgamation* sqlite-build >/dev/
 [ -e sqlite-build ] || exit 1
 
 mv sqlite-build build
-
-
-# cd sqlite-build
+cd build
 
 export NDK_PROJECT_PATH=$(pwd)
+export ANDROID_SDK_HOME="$ANDROID_SDK_ROOT"
+export ANDROID_NDK_ROOT="$ANDROID_SDK_ROOT/ndk/$ANDROID_NDK_VERSION"
+export PATH="$ANDROID_NDK_ROOT:$PATH"
 
 echo "curr dir : $NDK_PROJECT_PATH"
 
-find .
+ndk-build
